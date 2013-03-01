7dda6a47b4d1b99616c79515275486e8a1d2059e
Johny Lam <johny.lam@gmail.com>
2013-03-01 01:51:37 -0800

    add page to create a company

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,22 @@
 package controllers;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import models.Company;
+import models.SampleDocument;
 
+import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+import rss.RssFetcher;
+import rss.SimilarityResult;
+import views.html.index;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
@@ -20,14 +28,36 @@ public class Application extends Controller
 
 	public static Result index () throws IOException
 	{
+		return ok (index.render ("Hello"));
+	}
+	
+	private static void testPipeline() throws IOException
+	{
 		Company company = Company.getCompany ("sephora");
 		if (company == null)
 			company = new Company ("sephora");
+		HttpClient client = new DefaultHttpClient ();
+		System.out.println("Processing samples");
 		ProcessSampleDocuments processor = new ProcessSampleDocuments (
-				new DefaultHttpClient (), company);
+				client, company);
 		processor.processDocuments (Files.readLines (new File (
 				"resources/sample_articles.txt"), Charsets.UTF_8));
-		return ok ("foo");
+		System.out.println("Processing Rss feeds");
+		RssFetcher fetcher = new RssFetcher (company, client);
+		List<SimilarityResult> results = fetcher.getSimilarLinks (Files.readLines (new File("resources/rss_feeds.txt"), Charsets.UTF_8));
+		BufferedWriter writer = Files.newWriter (new File("out.txt"), Charsets.UTF_8);
+		for(SimilarityResult result: results)
+		{
+			writer.write (result.getLink ().getUrl ());
+			writer.newLine ();
+			Map<SampleDocument, Double> map =result.getSimilarityScores ();
+			for(SampleDocument doc: map.keySet ())
+			{
+				writer.write (doc.toString () + " "+ map.get (doc));
+				writer.newLine ();
+			}
+		}
+		writer.close ();
 	}
 
 }
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -10,6 +10,13 @@ create table company (
   constraint pk_company primary key (uid))
 ;
 
+create table rss_feed (
+  uid                       bigint not null,
+  company_uid               bigint,
+  url                       clob,
+  constraint pk_rss_feed primary key (uid))
+;
+
 create table sample_document (
   uid                       bigint not null,
   company_uid               bigint,
@@ -21,10 +28,14 @@ create table sample_document (
 
 create sequence company_seq;
 
+create sequence rss_feed_seq;
+
 create sequence sample_document_seq;
 
-alter table sample_document add constraint fk_sample_document_company_1 foreign key (company_uid) references company (uid) on delete restrict on update restrict;
-create index ix_sample_document_company_1 on sample_document (company_uid);
+alter table rss_feed add constraint fk_rss_feed_company_1 foreign key (company_uid) references company (uid) on delete restrict on update restrict;
+create index ix_rss_feed_company_1 on rss_feed (company_uid);
+alter table sample_document add constraint fk_sample_document_company_2 foreign key (company_uid) references company (uid) on delete restrict on update restrict;
+create index ix_sample_document_company_2 on sample_document (company_uid);
 
 
 
@@ -34,11 +45,15 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists company;
 
+drop table if exists rss_feed;
+
 drop table if exists sample_document;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists company_seq;
+
+drop sequence if exists rss_feed_seq;
 
 drop sequence if exists sample_document_seq;
 
